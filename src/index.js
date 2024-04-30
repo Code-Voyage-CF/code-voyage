82a2cf8914c5f1f7c6524cb67e64c238165e08f3
Patrick Kawika Reveira <patrick.k.reveira@gmail.com>
2024-04-30 10:26:08 -0700

    added code to export amadeus to other files

--- src/index.js
+++ src/index.js
@@ -1,20 +1,2 @@
-const Amadeus = require('amadeus');
+const amadeus = require('./config/amadeus.js');
 
-const dotenv = require('dotenv').config();
-
-const amadeus = new Amadeus({
-  clientId: process.env.AMADEUS_CLIENT_ID,
-  clientSecret: process.env.AMADEUS_CLIENT_SECRET,
-});
-
-
-amadeus.shopping.flightOffersSearch.get({
-    originLocationCode: 'SYD',
-    destinationLocationCode: 'BKK',
-    departureDate: '2024-06-21',
-    adults: '2'
-}).then(function(response){
-  console.log(response.data);
-}).catch(function(responseError){
-  console.log(responseError.code);
-});
