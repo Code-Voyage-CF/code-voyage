764add1ae79279c5b4b849e1a71c4a5951463401
Patrick Kawika Reveira <patrick.k.reveira@gmail.com>
2024-05-02 10:40:19 -0700

    commit to push changes to pull Ahmed branch

--- server.js
+++ server.js
@@ -2,20 +2,15 @@ const express = require("express")
 const dotenc = require('dotenv').config();
 const flightRoutes = require('./src/routes/flightsRoute.js');
 const shoppingRoutes = require('./src/routes/shoppingRoute.js');
-// const lodgingRoutes = require('./src/routes/lodgingRoute.js');
+const lodgingRoutes = require('./src/routes/lodgingRoute.js');
 
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
-// app.get('/', (req, res) => {
-//     res.send('Hello World!')
-// })
-
-
 app.use('/flights', flightRoutes);
 app.use('/shopping', shoppingRoutes);
-// app.use('/lodging', lodgingRoutes);
+app.use('/lodging', lodgingRoutes);
 
 app.listen(PORT, () => {
     console.log(`App is running on PORT ${PORT}`)
