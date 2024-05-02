ed06a6b48429b5823374de126877e7ff56a983c0
Ahmed Elnogoumi <ahmedelnogoumi@gmail.com>
2024-05-02 10:42:47 -0700

    Login working using route.

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 const express = require("express");
-const dotenc = require('dotenv').config();
+const dotenv = require('dotenv').config();
 const flightRoutes = require('./src/routes/flightsRoute.js');
 const shoppingRoutes = require('./src/routes/shoppingRoute.js');
 // const lodgingRoutes = require('./src/routes/lodgingRoute.js');
 const authRoute = require('./src/routes/authRoute.js');  // Include the authentication route
 
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Uncomment to enable the root route
-// app.get('/', (req, res) => {
-//     res.send('Hello World!')
-// });
-
+app.use(express.json());  // This should come before any routes are defined
+app.use('/auth', authRoute); 
 app.use('/flights', flightRoutes);
 app.use('/shopping', shoppingRoutes);
-app.use('/auth', authRoute); 
+
+// Uncomment to enable the root route
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
 
 app.listen(PORT, () => {
     console.log(`App is running on PORT ${PORT}`);
