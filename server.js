5f41fad5dfd30075512116332cc43caa95f07c18
Patrick Kawika Reveira <patrick.k.reveira@gmail.com>
2024-05-02 10:54:10 -0700

    committing for development branch pull

--- server.js
+++ server.js
@@ -2,16 +2,11 @@ const express = require("express");
 const dotenc = require('dotenv').config();
 const flightRoutes = require('./src/routes/flightsRoute.js');
 const shoppingRoutes = require('./src/routes/shoppingRoute.js');
-// const lodgingRoutes = require('./src/routes/lodgingRoute.js');
-const authRoute = require('./src/routes/authRoute.js');  // Include the authentication route
+const authRoute = require('./src/routes/authRoute.js');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Uncomment to enable the root route
-// app.get('/', (req, res) => {
-//     res.send('Hello World!')
-// });
 
 app.use('/flights', flightRoutes);
 app.use('/shopping', shoppingRoutes);
