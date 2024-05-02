67edc53cf7ed0918958e79ffac0e925348f7933e
Ahmed Elnogoumi <ahmedelnogoumi@gmail.com>
2024-05-01 22:42:14 -0700

    got login working via middleware

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-const express = require("express")
+const express = require("express");
 const dotenc = require('dotenv').config();
 const flightRoutes = require('./src/routes/flightsRoute.js');
 const shoppingRoutes = require('./src/routes/shoppingRoute.js');
 // const lodgingRoutes = require('./src/routes/lodgingRoute.js');
+const authRoute = require('./src/routes/authRoute.js');  // Include the authentication route
 
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-const app = express()
-const PORT = process.env.PORT || 3000
-
+// Uncomment to enable the root route
 // app.get('/', (req, res) => {
 //     res.send('Hello World!')
-// })
-
+// });
 
 app.use('/flights', flightRoutes);
 app.use('/shopping', shoppingRoutes);
-// app.use('/lodging', lodgingRoutes);
+app.use('/auth', authRoute); 
 
 app.listen(PORT, () => {
-    console.log(`App is running on PORT ${PORT}`)
+    console.log(`App is running on PORT ${PORT}`);
 });
--- src/middleware/authorize.js
+++ src/middleware/authorize.js
@@ -3,33 +3,36 @@
 const jwt = require('jsonwebtoken');
 const jwksClient = require('jwks-rsa');
 
-function verifyUser(request, response, next) {
-
-  function valid(err, user) {
-    if (err) {
-      next(err);
-    }
-    request.user = user;
-    next();
-  }
-
-  try {
-    const token = request.headers.authorization.split(' ')[1];
-    jwt.verify(token, getKey, {}, valid);
-  } catch (e) {
-    next('Not Authorized');
-  }
-}
-
 const client = jwksClient({
   jwksUri: process.env.JWKS_URI,
 });
 
 function getKey(header, callback) {
-  client.getSigningKey(header.kid, function(err, key) {
-    const signingKey = key.publicKey || key.rsaPublicKey;
-    callback(null, signingKey);
-  })
+  client.getSigningKey(header.kid, (err, key) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      const signingKey = key.publicKey || key.rsaPublicKey;
+      callback(null, signingKey);
+    }
+  });
+}
+
+function verifyUser(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({ error: 'No authorization token provided.' });
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  jwt.verify(token, getKey, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ error: 'Failed to authenticate token.' });
+    }
+    req.user = decoded;
+    next();
+  });
 }
 
 module.exports = verifyUser;
